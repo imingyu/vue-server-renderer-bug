462354e86fa1dac325a67c559540756da3243c96
imingyu <ymy48253@tcent.cn>
2017-10-10 19:07:58 +0800

    change

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const app = new Vue({
 const renderer = require('vue-server-renderer').createRenderer()
 renderer.renderToString(app, (err, html) => {
     if (err) throw err;//因为模板是不正确的，会报错，但是此处的err对象确实空的
-    console.log('渲染成功，内容为：' + html)
+    console.log('success：' + html)
 })
